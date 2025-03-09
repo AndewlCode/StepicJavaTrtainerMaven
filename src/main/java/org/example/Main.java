682b54560944d637be943f530e6d1fc03798eb6d
Andrey Lobachev <andrey_lobachev@internet.ru>
2025-03-09 12:09:16 +0300

    Project updated

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> tags = readInput();
+        HashSet<String> stringHashSet = new HashSet<>(tags);
+        List<String> uniqueData = new ArrayList<>(stringHashSet.stream().toList());
+        Collections.sort(uniqueData);
+        //StringBuilder stringBuilder = new StringBuilder(String.join(", ", uniqueData.toString())
+        //        .replace("[", "")
+        //        .replace("]", ""));
+        String result = String.join(", ", uniqueData);
+        System.out.println(result);
+    }
+
+
+    public static List<String> readInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        if (scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            Gson gson = new Gson();
+            String[] dataArray = gson.fromJson(input, String[].class);
+            return Arrays.asList(dataArray);
+        }
+        scanner.close();
+        return null;
+    }
+}
